de4998c717d97065a659b2fd0e08f0970fbcb2c8
Zoriglon Ayush <z.ayush@gmx.de>
2021-06-15 00:43:59 +0200

    added functionality to add cards to the board

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,18 +1,18 @@
 const cards = [
-    { name: 'tomberry', img: 'Tomberry.png', topAtk: 2, rightAtk: 2, bottomAtk: 7, leftAtk: 2 },
-    { name: 'sabotender', img: 'Sabotender.png', topAtk: 4, rightAtk: 3, bottomAtk: 3, leftAtk: 3 },
-    { name: 'morbol', img: 'Morbol.png', topAtk: 5, rightAtk: 2, bottomAtk: 5, leftAtk: 2 },
+    { name: 'tomberry', img: 'tomberry.png', topAtk: 2, rightAtk: 2, bottomAtk: 7, leftAtk: 2 },
+    { name: 'sabotender', img: 'sabotender.png', topAtk: 4, rightAtk: 3, bottomAtk: 3, leftAtk: 3 },
+    { name: 'morbol', img: 'morbol.png', topAtk: 5, rightAtk: 2, bottomAtk: 5, leftAtk: 2 },
     { name: 'cloud', img: 'cloud.png', topAtk: 9, rightAtk: 3, bottomAtk: 9, leftAtk: 8 },
-    { name: 'squall', img: 'Squall.png', topAtk: 6, rightAtk: 10, bottomAtk: 10, leftAtk: 1 },
-    { name: 'tomberry', img: 'Tomberry.png', topAtk: 2, rightAtk: 2, bottomAtk: 7, leftAtk: 2 },
-    { name: 'sabotender', img: 'Sabotender.png', topAtk: 4, rightAtk: 3, bottomAtk: 3, leftAtk: 3 },
-    { name: 'morbol', img: 'Morbol.png', topAtk: 5, rightAtk: 2, bottomAtk: 5, leftAtk: 2 },
-    { name: 'cloud', img: 'cloud.png', topAtk: 9, rightAtk: 3, bottomAtk: 9, leftAtk: 8 },
+    { name: 'squall', img: 'squall.png', topAtk: 6, rightAtk: 10, bottomAtk: 10, leftAtk: 1 },
+    { name: 'lightning', img: 'lightning.png', topAtk: 9, rightAtk: 1, bottomAtk: 9, leftAtk: 10 },
+    { name: 'adamanTaimai', img: 'adaman-taimai.png', topAtk: 5, rightAtk: 7, bottomAtk: 4, leftAtk: 4 },
+    { name: 'tiamat', img: 'tiamat.png', topAtk: 9, rightAtk: 6, bottomAtk: 5, leftAtk: 6 },
+    { name: 'behemoth', img: 'behemoth.png', topAtk: 7, rightAtk: 8, bottomAtk: 4, leftAtk: 2 },
     // { name: 'squall', img: 'Squall.png', topAtk: 6, rightAtk: 10, bottomAtk: 10, leftAtk: 1 },
     // { name: 'squall', img: 'Squall.png', topAtk: 6, rightAtk: 10, bottomAtk: 10, leftAtk: 1 },
     // { name: 'tomberry', img: 'Tomberry.png', topAtk: 2, rightAtk: 2, bottomAtk: 7, leftAtk: 2 },
     // { name: 'sabotender', img: 'Sabotender.png', topAtk: 4, rightAtk: 3, bottomAtk: 3, leftAtk: 3 },
     // { name: 'morbol', img: 'Morbol.png', topAtk: 5, rightAtk: 2, bottomAtk: 5, leftAtk: 2 },
     // { name: 'cloud', img: 'cloud.png', topAtk: 9, rightAtk: 3, bottomAtk: 9, leftAtk: 8 },
-    { name: 'squall', img: 'Squall.png', topAtk: 6, rightAtk: 10, bottomAtk: 10, leftAtk: 1 }
+    { name: 'chocomoppel', img: 'chocomoppel.png', topAtk: 5, rightAtk: 5, bottomAtk: 5, leftAtk: 5 }
   ];
--- scripts/main.js
+++ scripts/main.js
@@ -4,32 +4,49 @@ const triadGame = new TripleTriadGame(cards)
 window.addEventListener('load', (event) => {
     let player1CardsHtml  = '';
     let player2CardsHtml  = '';
-    console.log(document.getElementById('player1Cards'))
-    console.log(document.getElementById('player2Cards'))
+    // console.log(document.getElementById('player1Cards'))
+    // console.log(document.getElementById('player2Cards'))
+    console.log(triadGame.pickedCard[0])
+
     triadGame.cards.forEach((pic) => {
+        
         player1CardsHtml += `
-            <div class="card">
-            <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
-            </div>`;
+            <div class="card frontGreen" style="background-image: url(img/${pic.img})"> </div>`;
         triadGame.player1Cards.push(pic)
         
         player2CardsHtml += `
-            <div class="card">
-            <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
-            </div>`;
+            <div class="card frontRed" style="background-image: url(img/${pic.img})"> </div>`;
         triadGame.player2Cards.push(pic)
     });
     document.getElementById('player1Cards').innerHTML = player1CardsHtml;
-    
     document.getElementById('player2Cards').innerHTML = player2CardsHtml;
 
+
     document.querySelectorAll('.card').forEach((card) => {
         card.addEventListener('click', () => {
-
+            if( triadGame.pickedCard.length === 0){
+            triadGame.pickedCard.push(card)
+            }
+            console.log(triadGame.pickedCard[0])
         })   
     })
+    
+    document.getElementById('game-board').addEventListener('click', (arr) => {
+        console.log(triadGame.pickedCard[0])
+        console.log(arr.path[0])
+        console.log(arr.path[0]) 
+        if( triadGame.pickedCard.length === 1){
+            arr.path[0].appendChild(triadGame.pickedCard[0]) 
+            arr.path[0].querySelector('.frontRed').classList.remove('card')
+            arr.path[0].querySelector('.frontRed').classList.add('on-board-cards')
+            // arr.path[0].querySelector('.frontGreen').classList.remove('card')
+            // arr.path[0].querySelector('.frontGreen').classList.add('on-board-cards')
+            triadGame.pickedCard.pop();
+            console.log(triadGame.pickedCard[0])
+        }
+    })
 
-    document.querySelectorAll('.card').forEach((card) => {
-        card.addEventListener('click', () => {
+
+   
 
 });
