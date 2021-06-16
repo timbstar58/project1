cb4e463f1f55d854310246374bd4991c088ea63b
Zoriglon Ayush <z.ayush@gmx.de>
2021-06-16 13:25:41 +0200

    added attackmethod

--- scripts/game.js
+++ scripts/game.js
@@ -2,14 +2,225 @@ class TripleTriadGame {
 
     constructor(cards) {
         this.cards = cards;
-        this.player1Cards = [];
-        this.player2Cards = [];
-        this.pickedCardBlue= [];
+        this.playerGreenCards = [];
+        this.playerRedCards = [];
+        this.pickedCardGreen= [];
         this.pickedCardRed=[]
     }
 
 
-    checkIfStronger(card1, card2) {
+
+
+    checkIfStronger(arg) {
+        
+        // let oneOneAttackRight = document.querySelector('.field-one-one .on-board-cards').dataset.attackright
+        // let oneOneAttackBottom = document.querySelector('.field-one-one .on-board-cards').dataset.attackbottom
+        // let oneTwoAttackRight = document.querySelector('.field-one-two .on-board-cards').dataset.attackright
+        // let oneTwoAttackBottom = document.querySelector('.field-one-two .on-board-cards').dataset.attackbottom
+        // let oneTwoAttackLeft = document.querySelector('.field-one-two .on-board-cards').dataset.attackleft
+        // let oneThreeAttackLeft = document.querySelector('.field-one-three .on-board-cards').dataset.attackleft
+        // let oneThreeAttackBottom = document.querySelector('.field-one-three .on-board-cards').dataset.attackbottom
+
+        // let twoOneAttackTop = document.querySelector('.field-two-one .on-board-cards').dataset.attacktop
+        // let twoOneAttackRight = document.querySelector('.field-two-one .on-board-cards').dataset.attackright
+        // let twoOneAttackBottom = document.querySelector('.field-two-one .on-board-cards').dataset.attackbottom
+        // let twoTwoAttackTop = document.querySelector('.field-two-two .on-board-cards').dataset.attacktop
+        // let twoTwoAttackRight = document.querySelector('.field-two-two .on-board-cards').dataset.attackright
+        // let twoTwoAttackBottom = document.querySelector('.field-two-two .on-board-cards').dataset.attackbottom
+        // let twoTwoAttackLeft = document.querySelector('.field-two-two .on-board-cards').dataset.attackleft
+        // let twoThreeAttackTop = document.querySelector('.field-two-three .on-board-cards').dataset.attacktop
+        // let twoThreeAttackBottom = document.querySelector('.field-two-three .on-board-cards').dataset.attackbottom
+        // let twoThreeAttackLeft = document.querySelector('.field-two-three .on-board-cards').dataset.attackleft
+
+        // let threeOneAttackTop = document.querySelector('.field-three-one .on-board-cards').dataset.attacktop
+        // let threeOneAttackRight = document.querySelector('.field-three-one .on-board-cards').dataset.attackright
+        // let threeTwoAttackLeft = document.querySelector('.field-three-two .on-board-cards').dataset.attackleft
+        // let threeTwoAttackTop = document.querySelector('.field-three-two .on-board-cards').dataset.attacktop
+        // let threeTwoAttackRight = document.querySelector('.field-three-two .on-board-cards').dataset.attackright
+        // let threeThreeAttackLeft = document.querySelector('.field-three-three .on-board-cards').dataset.attackleft
+        // let threeThreeAttackTop = document.querySelector('.field-three-three .on-board-cards').dataset.attacktop
+        // console.log(arg.path[0].classList.value)
+        
+        switch (arg.path[0].classList.value) {
+            case 'box field-one-one':
+                console.log('field-one-one')
+                console.log(document.querySelector('.field-two-one .on-board-cards'))
+                console.log(document.querySelector('.field-one-two .on-board-cards'))
+    
+                if(document.querySelector('.field-two-one .on-board-cards') !== null){
+                    if(document.querySelector('.field-one-one .on-board-cards').dataset.attackbottom > document.querySelector('.field-two-one .on-board-cards').dataset.attacktop){ console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-one-two .on-board-cards') !== null){
+                    if(document.querySelector('.field-one-one .on-board-cards').dataset.attackright > document.querySelector('.field-one-two .on-board-cards').dataset.attackleft){console.log('flippy flappy')
+                    }
+                }
+                break;
+         
+            case 'box field-one-two':
+                console.log('field-one-two')
+                console.log(document.querySelector('.field-one-three .on-board-cards'))
+                console.log(document.querySelector('.field-two-two .on-board-cards'))
+                console.log(document.querySelector('.field-one-one .on-board-cards'))
+    
+                if(document.querySelector('.field-one-three .on-board-cards') !== null){
+                    if(document.querySelector('.field-one-two .on-board-cards').dataset.attackright > document.querySelector('.field-one-three .on-board-cards').dataset.attackleft){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-two-two .on-board-cards') !== null){
+                    if(document.querySelector('.field-one-two .on-board-cards').dataset.attackbottom > document.querySelector('.field-two-two .on-board-cards').dataset.attacktop){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-one-one .on-board-cards') !== null){
+                    if(document.querySelector('.field-one-two .on-board-cards').dataset.attackleft > document.querySelector('.field-one-one .on-board-cards').dataset.attackright){console.log('flippy flappy')
+                    }
+                }
+                break;
+
+            case 'box field-one-three':
+                console.log('field-one-three')
+                console.log(document.querySelector('.field-two-three .on-board-cards'))
+                console.log(document.querySelector('.field-one-two .on-board-cards'))
+    
+                if(document.querySelector('.field-two-three .on-board-cards') !== null){
+                    if(document.querySelector('.field-one-three .on-board-cards').dataset.attackbottom > document.querySelector('.field-two-three .on-board-cards').dataset.attacktop){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-one-two .on-board-cards') !== null){
+                    if(document.querySelector('.field-one-three .on-board-cards').dataset.attackleft > document.querySelector('.field-one-two .on-board-cards').dataset.attackright){console.log('flippy flappy')
+                    }
+                }
+                break;
+    
+            case 'box field-two-one' :
+                console.log('field-two-one')
+                if(document.querySelector('.field-two-two .on-board-cards') !== null){
+                    if(document.querySelector('.field-two-one .on-board-cards').dataset.attackright > document.querySelector('.field-two-two .on-board-cards').dataset.attackleft){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-one-one .on-board-cards')!== null){
+                    if(document.querySelector('.field-two-one .on-board-cards').dataset.attacktop > document.querySelector('.field-one-one .on-board-cards').dataset.attackbottom){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-three-one .on-board-cards')!== null){
+                    if(document.querySelector('.field-two-one .on-board-cards').dataset.attackbottom > document.querySelector('.field-three-one .on-board-cards').dataset.attacktop){console.log('flippy flappy')
+                    }
+                }
+                break;
+    
+            case 'box field-two-two':
+                console.log('field-two-two')
+                if(document.querySelector('.field-two-three .on-board-cards')!== null){
+                    if(document.querySelector('.field-two-two .on-board-cards').dataset.attackright > document.querySelector('.field-two-three .on-board-cards').dataset.attackleft){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-one-two .on-board-cards')!== null){
+                    if(document.querySelector('.field-two-two .on-board-cards').dataset.attacktop > document.querySelector('.field-one-two .on-board-cards').dataset.attackbottom){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-three-two .on-board-cards')!== null){
+                    if(document.querySelector('.field-two-two .on-board-cards').dataset.attackbottom > document.querySelector('.field-three-two .on-board-cards').dataset.attacktop){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-two-one .on-board-cards')!== null){
+                    if(document.querySelector('.field-two-two .on-board-cards').dataset.attackleft > document.querySelector('.field-two-one .on-board-cards').dataset.attackright){console.log('flippy flappy')
+                    }
+                }
+                break
+    
+            case 'box field-two-three':
+                console.log('field-two-three')
+                if(document.querySelector('.field-two-two .on-board-cards') !== null){
+                    if(document.querySelector('.field-two-three .on-board-cards').dataset.attackleft > document.querySelector('.field-two-two .on-board-cards').dataset.attackright){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-one-three .on-board-cards')!== null){
+                    if(document.querySelector('.field-two-three .on-board-cards').dataset.attacktop > document.querySelector('.field-one-three .on-board-cards').dataset.attackbottom){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-three-three .on-board-cards')!== null){
+                    if(document.querySelector('.field-two-three .on-board-cards').dataset.attackbottom > document.querySelector('.field-three-three .on-board-cards').dataset.attacktop){console.log('flippy flappy')
+                    }
+                }
+                break;
+            
+    
+            case 'box field-three-one':
+                console.log('field-three-one')
+                if(document.querySelector('.field-three-two .on-board-cards')!== null){
+                    if(document.querySelector('.field-three-one .on-board-cards').dataset.attackright > document.querySelector('.field-three-two .on-board-cards').dataset.attackleft){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-two-one .on-board-cards')!== null){
+                    if(document.querySelector('.field-three-one .on-board-cards').dataset.attacktop > document.querySelector('.field-two-one .on-board-cards').dataset.attackbottom){console.log('flippy flappy')
+                    }
+                }
+                break;
+    
+            case 'box field-three-two':
+                console.log('field-three-two')
+                if(document.querySelector('.field-three-one .on-board-cards')!== null){
+                    if (document.querySelector('.field-three-two .on-board-cards').dataset.attackleft > document.querySelector('.field-three-one .on-board-cards').dataset.attackright){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-two-two .on-board-cards')!== null){
+                    if(document.querySelector('.field-three-two .on-board-cards').dataset.attacktop > document.querySelector('.field-two-two .on-board-cards').dataset.attackbottom){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-three-three .on-board-cards')!== null){
+                    if(document.querySelector('.field-three-two .on-board-cards').dataset.attackright > document.querySelector('.field-three-three .on-board-cards').dataset.attackleft){console.log('flippy flappy')
+                    }
+                }
+                break
+    
+            case 'box field-three-three':
+                console.log('field-three-three')
+                if(document.querySelector('.field-three-two .on-board-cards')!== null){
+                    if(document.querySelector('.field-three-three .on-board-cards').dataset.attackleft > document.querySelector('.field-three-two .on-board-cards').dataset.attackright){console.log('flippy flappy')
+                    }
+                }
+                else if(document.querySelector('.field-two-three .on-board-cards')!== null){
+                    if(document.querySelector('.field-three-three .on-board-cards').dataset.attacktop > document.querySelector('.field-two-three .on-board-cards').dataset.attackbottom){console.log('flippy flappy')
+                    }
+                }
+                break
+            }
+
+        
+        // Field 1-1
+        // console.log(document.querySelector('.field-one-three .on-board-cards').dataset.attackbottom)
+
+        // if (parseInt.oneOneAttackBottom > parseInt.twoOneAttackTop || undefined){ console.log('flippy flappy')
+        // }
+        // else if (parseInt.oneOneAttackRight > parseInt.oneTwoAttackLeft || undefined){console.log('flippy flappy')
+        // }
+        // // Field 1-2
+        // else if (parseInt.oneTwoAttackRight > parseInt.oneThreeAttackLeft || undefined){console.log('flippy flappy')
+        // }
+        // else if (parseInt.oneTwoAttackBottom > parseInt.twoTwoAttackTop || undefined){console.log('flippy flappy')
+        // }
+        // // Field 1-3
+        // else if (parseInt.oneThreeAttackBottom > parseInt.twoThreeAttackTop|| undefined){console.log('flippy flappy')
+        // }
+        // // Field 2-1
+        // else if (parseInt.twoOneAttackRight > parseInt.twoTwoAttackLeft|| undefined ){console.log('flippy flappy')
+        // }
+        // else if (parseInt.twoOneAttackBottom > parseInt.threeOneAttackTop || undefined){console.log('flippy flappy')
+        // }
+        // // Field 2-2
+        // else if (parseInt.twoTwoAttackRight > parseInt.twoThreeAttackLeft || undefined){console.log('flippy flappy')
+        // }
+        // else if (parseInt.twoTwoAttackBottom > parseInt.threeTwoAttackTop || undefined){console.log('flippy flappy')
+        // }
+        // // Field 2-3
+        // else if (parseInt.twoThreeAttackBottom > parseInt.threeThreeAttackTop || undefined){console.log('flippy flappy')
+        // }
+        // // Field 3-1
+        // else if (parseInt.threeOneAttackRight > parseInt.threeTwoAttackLeft|| undefined ){ console.log('flippy flappy')
+        // }
+        // // Field 3-2
+        // else if (parseInt.threeTwoAttackRight > parseInt.threeThreeAttackLeft || undefined ){  console.log('flippy flappy')
+        // }
 
     }
 
--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,38 @@ const triadGame = new TripleTriadGame(cards)
 
 
 window.addEventListener('load', (event) => {
-    let player1CardsHtml  = '';
-    let player2CardsHtml  = '';
+    let playerGreenHtml  = '';
+    let playerRedHtml  = '';
 
     triadGame.cards.forEach((pic) => {
         
-        player1CardsHtml += `
-            <div class="card frontGreen" style="background-image: url(img/${pic.img})"> </div>`;
-        triadGame.player1Cards.push(pic)
+        playerGreenHtml += `
+            <div class="card frontGreen" style="background-image: url(img/${pic.img})" 
+                data-attackTop = "${pic.topAtk}"
+                data-attackRight = "${pic.rightAtk}"
+                data-attackBottom = "${pic.bottomAtk}"
+                data-attackLeft = "${pic.leftAtk}"
+            > </div>`
+            ;
+            
+        triadGame.playerGreenCards.push(pic)
         
-        player2CardsHtml += `
-            <div class="card frontRed" style="background-image: url(img/${pic.img})"> </div>`;
-        triadGame.player2Cards.push(pic)
+        playerRedHtml += `
+            <div class="card frontRed" style="background-image: url(img/${pic.img})"
+                data-attackTop = "${pic.topAtk}"
+                data-attackRight = "${pic.rightAtk}"
+                data-attackBottom = "${pic.bottomAtk}"
+                data-attackLeft = "${pic.leftAtk}"
+            > </div>`
+            ;
+        triadGame.playerRedCards.push(pic)
     });
-    document.getElementById('player1Cards').innerHTML = player1CardsHtml;
-    document.getElementById('player2Cards').innerHTML = player2CardsHtml;
+    document.getElementById('playerGreenCards').innerHTML = playerGreenHtml;
+    document.getElementById('playerRedCards').innerHTML = playerRedHtml;
 
-    // Player RED
+    // Player RED ---------------------------------------------------
         let turnCounter = 0
-        console.log(turnCounter)
+        // console.log(turnCounter)
         document.querySelectorAll('.frontRed').forEach((card) => {
             card.addEventListener('click', () => {
                 if( turnCounter %2 === 0){
@@ -36,31 +49,28 @@ window.addEventListener('load', (event) => {
                 arr.path[0].querySelector('.frontRed').classList.add('on-board-cards')
                 triadGame.pickedCardRed.length = 0;
                 turnCounter++;
-                console.log(turnCounter)
+                triadGame.checkIfStronger(arr);
             }
             else return false
         })
 
-        // Player BLUE
+    // Player Green---------------------------------------------------
         document.querySelectorAll('.frontGreen').forEach((card) => {
             card.addEventListener('click', () => {
                 if( turnCounter %2 !== 0){
-                triadGame.pickedCardBlue.push(card)
+                triadGame.pickedCardGreen.push(card)
                 }
             })   
         })
         
         document.getElementById('game-board').addEventListener('click', (arr) => {
-            console.log('string.length'+ triadGame.pickedCardBlue.length)
-            console.log('turned counter' + turnCounter)
-            if( triadGame.pickedCardBlue.length > 0 && turnCounter %2 !== 0){
-                
-                arr.path[0].appendChild(triadGame.pickedCardBlue[triadGame.pickedCardBlue.length -1]) 
+            if( triadGame.pickedCardGreen.length > 0 && turnCounter %2 !== 0){     
+                arr.path[0].appendChild(triadGame.pickedCardGreen[triadGame.pickedCardGreen.length -1]) 
                 arr.path[0].querySelector('.frontGreen').classList.remove('card')
                 arr.path[0].querySelector('.frontGreen').classList.add('on-board-cards')
-                triadGame.pickedCardBlue.length = 0;
+                triadGame.pickedCardGreen.length = 0;
                 turnCounter++;
-                console.log(turnCounter)
+                triadGame.checkIfStronger(arr);
             }
             else return false
         })
